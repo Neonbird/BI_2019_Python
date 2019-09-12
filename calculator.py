62b01be320fcadd2d367eb2767f52b4c60cb0304
Tanya Maslikova <kosha44@yandex.ru>
2019-09-12 20:53:51 +0300

    Добавлено описание задания в README.md и файл calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a = int(input())
+arifm = input()
+b = int(input())
+
+if arifm == '+':
+    print(a + b)
+elif arifm == '-':
+    print(a-b)
+elif arifm == '/':
+    print(a/b)
+elif arifm == '*':
+    print(a*b)
